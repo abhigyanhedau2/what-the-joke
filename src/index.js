15537081f05f587581af69568fb198ef724bd690
abhigyanhedau2 <93371184+abhigyanhedau2@users.noreply.github.com>
2022-09-14 23:17:52 +0530

    Add Favicon

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Favicon from 'react-favicon';
+import fav from '../src/assets/favicon.png';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Favicon url={fav} />
     <App />
   </React.StrictMode>
 );
