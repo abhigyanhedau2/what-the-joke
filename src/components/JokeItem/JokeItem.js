0bc25003adeb8811cde13d5a05e522bb5a722dad
abhigyanhedau2 <93371184+abhigyanhedau2@users.noreply.github.com>
2022-09-18 21:26:21 +0530

    Add Like Button and Send Liked Joke ID to Database

--- src/components/JokeItem/JokeItem.js
+++ src/components/JokeItem/JokeItem.js
@@ -3,11 +3,33 @@ import Card from '../UI/Card/Card';
 import classes from './JokeItem.module.css';
 
 const JokeItem = (props) => {
+
+    const addToFavorites = async () => {
+
+        try {
+            fetch('https://what-the-joke-6ab58-default-rtdb.firebaseio.com/favJokes.json', {
+                method: 'POST',
+                mode: 'no-cors',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    jokeId: props.id
+                })
+            });
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
     return (
         <Card className={classes.jokeItemWrapper}>
             <p><b>{props.setup}</b></p>
             <br />
             <p>{props.delivery}</p>
+            <div className={classes.actions}>
+                <i className="fa-regular fa-heart" onClick={addToFavorites}></i>
+            </div>
         </Card>
     )
 }
--- src/components/JokesList/JokesList.js
+++ src/components/JokesList/JokesList.js
@@ -6,15 +6,15 @@ const JokesList = (props) => {
 
     const jokes = props.jokesArr.map(item => {
         if (item.type === 'single')
-            return <JokeItem key={item.id} setup={item.joke} delivery="" />
+            return <JokeItem key={item.id} id={item.id} setup={item.joke} delivery="" />
         else
-            return <JokeItem key={item.id} setup={item.setup} delivery={item.delivery} />
+            return <JokeItem key={item.id} id={item.id} setup={item.setup} delivery={item.delivery} />
     })
 
     return (
         <Fragment>
             <div className={classes.jokeListWrapper}>{jokes}</div>
-            {!props.isLoading && <button className={classes.showMoreButton} onClick={props.onFetchMoreJokes}>Show More <i class="fa-solid fa-angle-down"></i> </button>}
+            {!props.isLoading && <button className={classes.showMoreButton} onClick={props.onFetchMoreJokes}>Show More <i className="fa-solid fa-angle-down"></i> </button>}
         </Fragment>
     )
 };
