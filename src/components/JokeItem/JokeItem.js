b50caeff0c7b33de920ec2fde57af49c8a5406b1
abhigyanhedau2 <93371184+abhigyanhedau2@users.noreply.github.com>
2022-09-30 17:00:05 +0530

    Revert "Add Joke ID in favJokesArr and send array to Firebase"
    This reverts commit 66d72693e53e28b00eb9bc895d9256cbffd3abad.

--- src/components/JokeItem/JokeItem.js
+++ src/components/JokeItem/JokeItem.js
@@ -4,13 +4,31 @@ import classes from './JokeItem.module.css';
 
 const JokeItem = (props) => {
 
+    const addToFavorites = async () => {
+
+        try {
+            fetch('https://what-the-joke-6ab58-default-rtdb.firebaseio.com/favJokes.json', {
+                method: 'POST',
+                mode: 'no-cors',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    jokeId: props.id
+                })
+            });
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
     return (
         <Card className={classes.jokeItemWrapper}>
             <p><b>{props.setup}</b></p>
             <br />
             <p>{props.delivery}</p>
             <div className={classes.actions}>
-                <i className="fa-regular fa-heart" onClick={props.onAddToFavorites}></i>
+                <i className="fa-regular fa-heart" onClick={addToFavorites}></i>
             </div>
         </Card>
     )
